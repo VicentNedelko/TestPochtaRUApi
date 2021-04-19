4188ca0671ae19565b3e29f5a6d2bfea367c1e14
Vicent Niad <glory.jly@gmail.com>
2021-04-19 17:49:56 +0300

    refactor : improve methods

--- TestPochtaRUApi/Program.cs
+++ TestPochtaRUApi/Program.cs
@@ -17,6 +17,9 @@ namespace TestPochtaRUApi
             Uri baseURI = new Uri("https://otpravka-api.pochta.ru");
             Uri uri = new Uri(baseURI, "postoffice/1.0/by-address");
 
+
+            //
+
             var builder = new UriBuilder(uri)
             {
                 Port = -1
@@ -52,40 +55,39 @@ namespace TestPochtaRUApi
 
             //
 
-            //var addressToDetailed_1 = new RequestDetalization
-            //{
-            //    id = "adr 1",
-            //    OriginalAddress = "Загородное шоссе, 7а, Москва",
-            //};
 
-            //var addressToDetailed_2 = new RequestDetalization
-            //{
-            //    id = "adr 2",
-            //    OriginalAddress = "Москва, корп. 1, Севастопольский пр., 42",
-            //};
+            var listAddresses = new List<RequestDetalization>
+            {
+                new RequestDetalization
+                {
+                    id = "adr 1",
+                    OriginalAddress = "Загородное шоссе, 7а, Москва",
+                },
+                new RequestDetalization
+                {
+                    id = "adr 2",
+                    OriginalAddress = "Москва, корп. 1, Севастопольский пр., 42",
+                },
+            };
 
-            //var listAddresses = new List<RequestDetalization>();
-            //listAddresses.Add(addressToDetailed_1);
-            //listAddresses.Add(addressToDetailed_2);
+            var serializedRequest = JsonConvert.SerializeObject(listAddresses);
+            var content = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
+            var requestDetailes = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(baseURI, "1.0/clean/address"),
+                Headers =
+                {
+                    { "Authorization", "AccessToken x9nATG1tt3do9ud8QgHcFp2JdyZP8RyL" },
+                    { "X-User-Authorization","Basic  eS1rdW5hc2hAeWFuZGV4LnJ1OjFxYXohUUFa" },
+                },
+                Content = content,
+            };
 
-            //var serializedRequest = JsonConvert.SerializeObject(listAddresses);
-            //var content = new StringContent(serializedRequest, Encoding.UTF8, "application/json");
-            //var requestDetailes = new HttpRequestMessage
-            //{
-            //    Method = HttpMethod.Post,
-            //    RequestUri = new Uri(baseURI, "1.0/clean/address"),
-            //    Headers =
-            //    {
-            //        { "Authorization", "AccessToken x9nATG1tt3do9ud8QgHcFp2JdyZP8RyL" },
-            //        { "X-User-Authorization","Basic  eS1rdW5hc2hAeWFuZGV4LnJ1OjFxYXohUUFa" },
-            //    },
-            //    Content = content,
-            //};
+            using var responseDetalization = await client.SendAsync(requestDetailes);
+            var detalization = JsonConvert.DeserializeObject<List<ResponseDetalization>>(
+                responseDetalization.Content.ReadAsStringAsync().Result);
 
-            //using var responseDetalization = await client.SendAsync(requestDetailes);
-            //var detalization = JsonConvert.DeserializeObject<List<ResponseDetalization>>(
-            //    responseDetalization.Content.ReadAsStringAsync().Result);
-            
 
 
             //
